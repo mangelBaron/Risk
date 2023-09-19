caaea9099dd541e559bfb2d74f67a0780e7b7331
mangelBaron <miguelbaronm@hotmail.com>
2023-09-19 18:40:03 -0500

    RISK COMPONENTE 2 (CORREGIDO)

---
+++ Ficha.h
@@ -0,0 +1,37 @@
+#ifndef ESTRUCT_COMP_FICHA_H
+#define ESTRUCT_COMP_FICHA_H
+
+
+
+class Ficha{
+    private:
+        std::string nombre;
+        int infanterias;
+    public:
+
+    //Constructores
+    Ficha(const std::string &nombre, int infanterias) : nombre(nombre), infanterias(infanterias) {}
+
+    //Metodos de acceso
+    const std::string &getNombre() const {
+        return nombre;
+    }
+
+    void setNombre(const std::string &nombre) {
+        Ficha::nombre = nombre;
+    }
+
+    int getInfanterias() const {
+        return infanterias;
+    }
+
+    void setInfanterias(int infanterias) {
+        Ficha::infanterias = infanterias;
+    }
+
+    //METODOS
+    void displayInfo();
+
+};
+
+#endif
