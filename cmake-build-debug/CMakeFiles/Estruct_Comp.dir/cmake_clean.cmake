e47e09eba48e023bf2305156125d0d847b80c0f4
mangelBaron <miguelbaronm@hotmail.com>
2023-10-12 19:43:58 -0500

    RISK ENTREGA PROYECTO ARCHIVOS

---
+++ cmake-build-debug/CMakeFiles/Estruct_Comp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Estruct_Comp.dir/main.cpp.obj"
+  "CMakeFiles/Estruct_Comp.dir/main.cpp.obj.d"
+  "Estruct_Comp.exe"
+  "Estruct_Comp.exe.manifest"
+  "Estruct_Comp.pdb"
+  "libEstruct_Comp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Estruct_Comp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
